98ec3ec3adb1e6f79b2ad271eff87a4e45e86f24
Jefferson Santos <jeff.debian2016@gmail.com>
2022-05-13 08:10:03 -0300

    Exercicios sobre imagens.

---
+++ 2_ListasDeExercicios/2_Images/AppAtividadeImages/AppAtividadeImages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppAtividadeImages
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("-------------- Treinamento Docker -------------");
+    }
+  }
+}
